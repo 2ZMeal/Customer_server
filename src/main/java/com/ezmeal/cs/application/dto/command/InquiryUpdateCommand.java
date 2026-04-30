db1c5252ffc06c3286bdb8420208bfdad210a3b2
hjk2132 <xiest@naver.com>
2026-05-01 01:25:36 +0900

    refactor: updateCommand 수정

--- src/main/java/com/ezmeal/cs/application/dto/command/InquiryUpdateCommand.java
+++ src/main/java/com/ezmeal/cs/application/dto/command/InquiryUpdateCommand.java
@@ -1,7 +1,6 @@
 package com.ezmeal.cs.application.dto.command;
 
 import com.ezmeal.common.enums.Role;
-import com.ezmeal.cs.domain.enums.AnswerStatus;
 import java.util.UUID;
 
 public record InquiryUpdateCommand(
